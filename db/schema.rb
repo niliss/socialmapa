f1713575d21614a0c1dbf87eb74c7bf374abfcaf
juanfrc <juanfrc2@gmail.com>
2015-04-11 16:24:21 -0500

    Add volunteer model, controller and nested routes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150411180105) do
+ActiveRecord::Schema.define(version: 20150411203050) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
@@ -40,5 +40,13 @@ ActiveRecord::Schema.define(version: 20150411180105) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "volunteers", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "event_id"
+  end
 
 end
