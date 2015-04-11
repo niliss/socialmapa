899c8dce973fe71ebf53009b103cf01aac19c3db
niliss <siskens@gmail.com>
2015-04-10 19:00:27 -0500

    initial after devise

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,22 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+end
